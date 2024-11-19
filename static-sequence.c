3416e1039b73ced685c4305dd24d5ba69a573336
Aung Yee <lyndon.lso@gmail.com>
2024-11-19 23:14:13 +0000

    update documentation

--- static-sequence.c
+++ static-sequence.c
@@ -2,7 +2,7 @@
 
 A static sequence is a sequence of fix length.
 It implements the following interface -
-    1. build(n) - build a sequence of size n
+    1. build(n, s) - build a sequence of n item, each is s byte
     2. len_sequence(s) - get the length of sequence s
     3. get_at(s, i) - get the element at index i
     4. set_at(s, i, x) - set the element at index i to x
@@ -28,16 +28,16 @@ static_sequence *build(size_t n, size_t size) {
     return a;
 }
 
-void destory(static_sequence * s) {
+void destory(static_sequence *s) {
     free(s->sequence);
     free(s);
 }
 
-size_t len_sequence(static_sequence * s) {
+size_t len_sequence(static_sequence *s) {
     return s->length;
 }
 
-void *get_at(static_sequence * s, size_t i) {
+void *get_at(static_sequence *s, size_t i) {
     if (i >= len_sequence(s)) {
         printf("get_at: Index %zu is out of bound for sequence s. \n", i);
         exit(1);
@@ -46,7 +46,7 @@ void *get_at(static_sequence * s, size_t i) {
     }
 }
 
-void set_at(static_sequence * s, size_t i, void *x) {
+void set_at(static_sequence *s, size_t i, void *x) {
     if (i >= len_sequence(s)) {
         printf("set_at: Index %zu is out of bound for sequence s. \n", i);
         exit(1);
